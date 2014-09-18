462985436f997d69b802c59c8d41c086646fb7be
Nicolas PERU <nicolas.peru@sonarsource.com>
2014-09-18 14:30:50 +0200

    Transform text file to fit file

--- src/main/java/com/garmin/fit/WorkoutCreator.java
+++ src/main/java/com/garmin/fit/WorkoutCreator.java
@@ -1,6 +1,8 @@
 package com.garmin.fit;
 
-import java.util.ArrayList;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -9,11 +11,12 @@ public class WorkoutCreator {
     System.out.println("FIT Encode Example Application");
 
     FileEncoder encode;
-
+    String filename = args[0];
+    java.io.File file = new java.io.File(filename);
     try {
-      encode = new FileEncoder(new java.io.File("/home/benzonico/Development/Garmin/semaine4.fit"));
+      encode = new FileEncoder(new java.io.File(file.getParentFile(), file.getName()+".fit"));
     } catch (FitRuntimeException e) {
-      System.err.println("Error opening file test.fit");
+      System.err.println("Error opening file "+filename+".fit");
       return;
     }
 
@@ -29,75 +32,107 @@ public class WorkoutCreator {
     fileCreatorMesg.setSoftwareVersion(300);
     encode.write(fileCreatorMesg);
 
-    WorkoutMesg workoutMesg = new WorkoutMesg();
-    List<WorkoutStepMesg> workoutStepMesgs = new ArrayList<WorkoutStepMesg>();
-    WorkoutStepMesg step = new WorkoutStepMesg();
-    step.setDurationTime(1200.0f);
-    step.setDurationType(WktStepDuration.TIME);
-    step.setTargetType(WktStepTarget.OPEN);
-    step.setIntensity(Intensity.WARMUP);
-    workoutStepMesgs.add(step);
+    try {
+      List<String> lines = FileUtils.readLines(file);
+      int lineNb = 0;
+      for (String line : lines) {
+        if (lineNb == 0) {
+          int stepNb = lines.size() - 1;
+          encode.write(createWorkoutMsg(line, stepNb));
+        } else {
+          WorkoutStepMesg workoutStep = createWorkoutStep(line);
+          workoutStep.setMessageIndex(lineNb - 2);
+          encode.write(workoutStep);
+        }
 
-    step = new WorkoutStepMesg();
-    step.setDurationType(WktStepDuration.DISTANCE);
-    step.setDurationDistance(200f);
-    step.setTargetType(WktStepTarget.OPEN);
-    step.setIntensity(Intensity.ACTIVE);
-    workoutStepMesgs.add(step);
-
-    step = new WorkoutStepMesg();
-    step.setDurationType(WktStepDuration.TIME);
-    step.setDurationTime(30.0f);
-    step.setTargetType(WktStepTarget.OPEN);
-    step.setIntensity(Intensity.REST);
-    workoutStepMesgs.add(step);
-
-    step = new WorkoutStepMesg();
-    step.setRepeatSteps(10L);
-    step.setDurationType(WktStepDuration.REPEAT_UNTIL_STEPS_CMPLT);
-    step.setDurationStep(1L);
-    workoutStepMesgs.add(step);
-
-    step = new WorkoutStepMesg();
-    step.setDurationType(WktStepDuration.TIME);
-    step.setDurationTime(150.0f);
-    step.setTargetType(WktStepTarget.OPEN);
-    step.setIntensity(Intensity.REST);
-    workoutStepMesgs.add(step);
-
-    step = new WorkoutStepMesg();
-    step.setRepeatSteps(2L);
-    step.setDurationType(WktStepDuration.REPEAT_UNTIL_STEPS_CMPLT);
-    step.setDurationStep(1L);
-    workoutStepMesgs.add(step);
-
-    step = new WorkoutStepMesg();
-    step.setDurationType(WktStepDuration.OPEN);
-    step.setTargetType(WktStepTarget.OPEN);
-    step.setIntensity(Intensity.WARMUP);
-    workoutStepMesgs.add(step);
-
-    workoutMesg.setWktName("Semaine 4");
-    workoutMesg.setNumValidSteps(workoutStepMesgs.size());
-    workoutMesg.setSport(Sport.RUNNING);
-    encode.write(workoutMesg);
-
-    int i = 0;
-    for (WorkoutStepMesg workoutStepMesg : workoutStepMesgs) {
-      workoutStepMesg.setMessageIndex(i);
-      i++;
-      encode.write(workoutStepMesg);
+        lineNb++;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
 
     try {
       encode.close();
-    } catch(FitRuntimeException e) {
+    } catch (FitRuntimeException e) {
       System.err.println("Error closing encode.");
       return;
     }
 
-    System.out.println("Encoded FIT file test.fit.");
+    System.out.println("Encoded FIT file "+filename+".fit.");
+  }
+
+  private static WorkoutStepMesg createWorkoutStep(String line) {
+    WorkoutStepMesg step = new WorkoutStepMesg();
+    if (line.startsWith("open")) {
+      step.setDurationType(WktStepDuration.OPEN);
+      step.setTargetType(WktStepTarget.OPEN);
+    } else if (line.startsWith("repeat")) {
+      String[] repeatInstruction = line.split(" ");
+      step.setRepeatSteps(Long.parseLong(repeatInstruction[1]));
+      step.setDurationType(WktStepDuration.REPEAT_UNTIL_STEPS_CMPLT);
+      long stepToLoopBackTo = Long.parseLong(repeatInstruction[2]) - 2; //-2 because index start at 0 and there is title in 1st line
+      step.setDurationStep(stepToLoopBackTo);
+    } else {
+      String[] stepInstructions = line.split(" ");
+      Unit unit = Unit.getUnit(stepInstructions[1]);
+      if (unit == null) {
+        throw new UnsupportedOperationException("Unit was not found");
+      }
+      unit.setValue(stepInstructions[0], step);
+      step.setTargetType(WktStepTarget.OPEN);
+    }
+    return step;
+  }
+
+  private static enum Unit {
+    MIN("min", WktStepDuration.TIME) {
+      @Override
+      public Float getValue(String value) {
+        return Float.parseFloat(value) * 60;
+      }
+    },
+    SECONDS("seconds", WktStepDuration.TIME),
+    METERS("meters", WktStepDuration.DISTANCE);
+
+    private String unitName;
+    private WktStepDuration wktStepDuration;
+
+    private Unit(String unitName, WktStepDuration wktStepDuration) {
+      this.unitName = unitName;
+      this.wktStepDuration = wktStepDuration;
+    }
+
+    public void setValue(String value, WorkoutStepMesg step) {
+      step.setDurationType(wktStepDuration);
+      if (wktStepDuration.equals(WktStepDuration.DISTANCE)) {
+        step.setDurationDistance(getValue(value));
+      } else if (wktStepDuration.equals(WktStepDuration.TIME)) {
+        step.setDurationTime(getValue(value));
+      }
+
+    }
+
+    public Float getValue(String value) {
+      return Float.parseFloat(value);
+    }
+
+    public static Unit getUnit(String unitName) {
+      for (Unit unit : values()) {
+        if (unit.unitName.equals(unitName)) {
+          return unit;
+        }
+      }
+      return null;
+    }
+
+  }
+
+  private static WorkoutMesg createWorkoutMsg(String line, int stepNumber) {
+    WorkoutMesg workoutMesg = new WorkoutMesg();
+    workoutMesg.setWktName(line);
+    workoutMesg.setNumValidSteps(stepNumber);
+    workoutMesg.setSport(Sport.RUNNING);
+    return workoutMesg;
   }
 
 }
